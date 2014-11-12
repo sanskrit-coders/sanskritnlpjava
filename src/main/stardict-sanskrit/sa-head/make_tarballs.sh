592f42cb2ddbdd05db56f975dac5080a9f3a5800
Vishvas Vasuki <vvasuki@google.com>
2014-11-11 19:05:48 -0800

    asdf

--- src/main/stardict-sanskrit/sa-head/make_tarballs.sh
+++ src/main/stardict-sanskrit/sa-head/make_tarballs.sh
@@ -1,7 +1,8 @@
-for dir in A*/
+for dir in */
 do
 	base=$(basename "$dir")
 	cd $dir
-	tar -czf "${base}.tar.gz" `ls *.idx *.dict *.ifo`
+	tar -czf "${base}.tar.gz" `ls *.idx *.dict *.ifo *.syn`
+	mv "${base}.tar.gz" ../tars/
 	cd ..
 done

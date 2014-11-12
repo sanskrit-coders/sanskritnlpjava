9b7951b7bc2f8fb4b28328464132b9264f7e3203
Vishvas Vasuki <vvasuki@google.com>
2014-11-11 18:57:23 -0800

    asdf

--- src/main/stardict-sanskrit/sa-head/AkhyAtachandrikA/mk_tsv.py
+++ src/main/stardict-sanskrit/sa-head/AkhyAtachandrikA/mk_tsv.py
@@ -2,14 +2,14 @@
 
 import csv
 
-with open('AkhyAtachandrikA.txt', 'rb') as csvfile:
+with open('AkhyAtachandrikA.txt', 'rU') as csvfile:
 	for roots in csv.reader(csvfile, delimiter=','):
 		if len(roots) < 3:
 			continue
 		verse_position = roots[0]
 		meaning = roots[1]
 		for root in roots[2:]:
-			print root + " " + meaning + " " + verse_position + "\t" + meaning + ' '.join(roots[2:])
+			print root + "\t" + meaning + " " + verse_position + " "  + ' '.join(roots[2:])
 			pass
-		print meaning + "\t" + meaning + ' '.join(roots[2:]) + " " + verse_position
+		print meaning + "\t" + meaning +  " " + verse_position + " "  + ' '.join(roots[2:])
 

4348e2603fa93306a778d95270837c955f8cf118
vvasuki <vvasuki@localhost>
2014-11-13 03:33:54 -0800

    asdf

--- src/main/stardict-sanskrit/sa-head/make_tarballs.sh
+++ src/main/stardict-sanskrit/sa-head/make_tarballs.sh
@@ -1,3 +1,6 @@
+touch tars/tars.txt
+rm tars/tars.txt
+touch tars/tars.txt
 for dir in */
 do
 	base=$(basename "$dir")
@@ -5,4 +8,7 @@ do
 	tar -czf "${base}.tar.gz" `ls *.idx *.dict *.ifo *.syn`
 	mv "${base}.tar.gz" ../tars/
 	cd ..
+	if [ ! -f "$dir/${base}.tar.gz" ]; then
+	  echo "https://github.com/vvasuki/sanskritnlp/raw/master/src/main/stardict-sanskrit/sa-head/tars/${base}.tar.gz" >> tars/tars.txt
+	fi
 done

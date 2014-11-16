0ebd1d9857e991f77b1a0004b9304bbcad507afd
vvasuki <vvasuki@localhost>
2014-11-16 07:05:56 -0800

    asdf

--- src/main/android-apps/SanskritDictionaryUpdater/app/src/main/java/sanskritcode/sanskritdictionaryupdater/MainActivity.java
+++ src/main/android-apps/SanskritDictionaryUpdater/app/src/main/java/sanskritcode/sanskritdictionaryupdater/MainActivity.java
@@ -1,5 +1,6 @@
 package sanskritcode.sanskritdictionaryupdater;
 
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -24,15 +26,66 @@ import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-    private static final String TAG = "MainActivity";
+    private static final String MAIN_ACTIVITY = "MainActivity";
     private static final String DICTIONARY_LIST_URL = "https://raw.githubusercontent.com/vvasuki/stardict-sanskrit/master/sa-head/tars/tars.MD";
     private static final String DICTIONARY_LOCATION = "dict";
     private static final String DOWNLOAD_LOCATION = "dict";
+
+    private TextView topText;
+    public List<String> dictUrls;
+
+    public class DictUrlGetter extends AsyncTask<String, Integer, Integer> {
+        private final String DICT_URL_GETTER = DictUrlGetter.class.getName();
+
+        @Override
+        public Integer doInBackground(String... dictionaryListUrls) {
+            Log.i(DICT_URL_GETTER, "Will get dictionaries from " + dictionaryListUrls.length + " urls:");
+            dictUrls = new ArrayList();
+            for(String url: dictionaryListUrls) {
+                Log.i(DICT_URL_GETTER, url);
+                try {
+                    DefaultHttpClient httpclient = new DefaultHttpClient();
+                    HttpGet httppost = new HttpGet(url);
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity ht = response.getEntity();
+
+                    BufferedHttpEntity buf = new BufferedHttpEntity(ht);
+
+                    InputStream is = buf.getContent();
+                    BufferedReader r = new BufferedReader(new InputStreamReader(is));
+
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        String dictUrl = line.replace("<", "").replace(">", "");
+                        dictUrls.add(dictUrl);
+                        Log.d(DICT_URL_GETTER, "Added dictionary: " + dictUrl);
+                        publishProgress(dictUrls.size());
+                    }
+                } catch (IOException e) {
+                    Log.e(DICT_URL_GETTER, "Failed " + e.getStackTrace());
+                }
+            }
+            Log.i(DICT_URL_GETTER, "Added so many dictionary urls: " + dictUrls.size());
+            return dictUrls.size();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            topText.setText("Got " + dictUrls.size() + " urls so far.");
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            topText.setText("Got " + dictUrls.size() + " urls. Now we download the dictionaries.");
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        topText = (TextView) findViewById(R.id.textView);
     }
 
 
@@ -58,34 +111,10 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public List<String> getDictUrls() {
-        Log.i(TAG, "Will get dictionaries from " + DICTIONARY_LIST_URL);
-        List<String> lstUrls = new ArrayList();
-        try {
-            DefaultHttpClient httpclient = new DefaultHttpClient();
-            HttpGet httppost = new HttpGet(DICTIONARY_LIST_URL);
-            HttpResponse response = httpclient.execute(httppost);
-            HttpEntity ht = response.getEntity();
-
-            BufferedHttpEntity buf = new BufferedHttpEntity(ht);
-
-            InputStream is = buf.getContent();
-            BufferedReader r = new BufferedReader(new InputStreamReader(is));
-
-            String line;
-            while ((line = r.readLine()) != null) {
-                String url = line.replace("<", "").replace(">", "");
-                lstUrls.add(url);
-                Log.d(TAG, url);
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Failed " + e.getStackTrace());
-        }
-        return lstUrls;
-    }
 
     public void putDictionaries(View v) {
+        DictUrlGetter dictUrlGetter = new DictUrlGetter();
+        dictUrlGetter.execute(DICTIONARY_LIST_URL);
         File sdcard = Environment.getExternalStorageDirectory();
-        List<String> dictUrls = getDictUrls();
     }
 }

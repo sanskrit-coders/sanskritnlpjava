2fff7afd392560159a08f185246f5007d0ec87fe
vvasuki <vishvas.vasuki@gmail.com>
2016-02-18 12:01:54 -0800

    asdf

--- src/main/scala/sanskritnlp/transliteration/optitrans.scala
+++ src/main/scala/sanskritnlp/transliteration/optitrans.scala
@@ -40,7 +40,7 @@ object optitrans extends RomanScript {
   override val romanToDevaConsonantsNoVirama = romanToDevaConsonants.mapValues(_.replaceAll("(.+)्$", "$1"))
   override val romanToDevaContextFreeReplacements = Map(
     "M" -> "ं",  "H" -> "ः",
-    "." -> "।", ".." -> "॥", "|" -> "।",  ".a" -> "ऽ",
+    "." -> "।", ".." -> "॥", ".a" -> "ऽ",
     "0" -> "०", "1"-> "१", "2"-> "२",
     "3"-> "३", "4"-> "४", "5"-> "५",
     "6"-> "६", "7"-> "७", "8"-> "८", "9"-> "९", "OM" -> "ॐ")

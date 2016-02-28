3546868e8d72d44d5c82afda4c1fea29b8452c67
vvasuki <vishvas.vasuki@gmail.com>
2016-02-27 19:29:55 -0800

    asdf

--- src/main/scala/sanskritnlp/bot/Wiktionary.scala
+++ src/main/scala/sanskritnlp/bot/Wiktionary.scala
@@ -1,22 +1,32 @@
 package sanskritnlp.bot
 
 import sanskritnlp.app._
-//import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot
-//import net.sourceforge.jwbf.core.contentRep.SimpleArticle
+import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot
+import net.sourceforge.jwbf.core.contentRep.SimpleArticle
 import org.slf4j.LoggerFactory
 
-class Wiktionary {
-val log = LoggerFactory.getLogger(this.getClass)
+object Wiktionary {
+  val log = LoggerFactory.getLogger(this.getClass)
 
-val userName = sanskritNlp.props.getProperty("WIKI_USER_NAME")
-val passwd = sanskritNlp.props.getProperty("WIKI_PASSWORD")
-log info userName + ":" + passwd
-//val bot = new MediaWikiBot("http://sa.wikipedia.org/w/")
-//bot.login(userName, passwd)
-//
-//def test = {
-//  val article = bot.readContent("Wiktionary:Sandbox")
-//  log info(article.getText());
-//}
+  val userName = sanskritNlp.props.getProperty("WIKI_USER_NAME")
+  // न भेतव्यम् इति केन दीक्षितेनोक्तम्?
+  val passwd = "nIlakaNTha"
+  val bot_wiktionary = new MediaWikiBot("http://sa.wiktionary.org/w/")
 
+  def login = {
+    log info userName + ":" + passwd
+    bot_wiktionary.login(userName, passwd)
+  }
+
+  def test = {
+    val article = bot_wiktionary.readData("Wiktionary:Sandbox123")
+    log info article.getText()
+    article.setText("यन्त्रेणेदं लिखितम्")
+    log info article.getText()
+  }
+
+  def main(args: Array[String]): Unit = {
+    login
+    test
+  }
 }

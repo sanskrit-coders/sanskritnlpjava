760b7c7e0cab7d0e6d6be592ea07b86c5b9f81e9
vvasuki <vishvas.vasuki@gmail.com>
2016-03-20 15:39:22 -0700

    asdf

--- src/main/scala/sanskritnlp/dictionary/BabylonDictionary.scala
+++ src/main/scala/sanskritnlp/dictionary/BabylonDictionary.scala
@@ -2,14 +2,17 @@ package sanskritnlp.dictionary
 
 import java.io.{File, PrintWriter}
 
+import org.slf4j.LoggerFactory
 import sanskritnlp.wiki.bot.wiktionary._
 
+import scala.collection.mutable.ListBuffer
 import scala.collection.immutable.HashMap
 import scala.collection.mutable
 import scala.io.{BufferedSource, Source}
 
 class BabylonDictionary(name_in: String, source_in: String = "") {
-  var wordToLocation = new HashMap[String, Int]
+  var wordToLocations = new HashMap[String, ListBuffer[Int]]
+  val log = LoggerFactory.getLogger(this.getClass)
 
   val dict_name = name_in
   val source = source_in
@@ -17,11 +20,15 @@ class BabylonDictionary(name_in: String, source_in: String = "") {
 
   // Maintained by external users of this dictionary - not here. Kept here for conveneince.
   val wordsSeen = new mutable.HashSet[String]
+  var start_word_index: Int = 1
+  var end_word_index: Int = 100000
+  var word_index = 0
 
   var linesIter: Iterator[String] = null
 
   def fromFile(infileStr: String) = {
     fileLocation = infileStr
+    word_index = 0
     val src = Source.fromFile(infileStr, "utf8")
     linesIter = src.getLines
   }
@@ -36,16 +43,52 @@ class BabylonDictionary(name_in: String, source_in: String = "") {
     return returnTuple
   }
 
+  def take(entriesToSkip: Int) = {
+    while(hasNext() && word_index < entriesToSkip) {
+      next()
+      word_index = word_index + 1
+    }
+  }
+
   def makeWordToLocationMap() = {
-    var word_index = 0
+    word_index = 0
     while (hasNext()) {
       val (headwords, meaning) = next()
       word_index = word_index + 1
       // log.info(s"word_index : $word_index")
       val sktHeadwords = headwords.filter(_ matches "\\p{IsDevanagari}+")
       sktHeadwords.foreach(word => {
-        wordToLocation += (word -> word_index)
+        val locus_list = wordToLocations.getOrElse(word, ListBuffer[Int]())
+        locus_list += word_index
+        wordToLocations += (word -> locus_list)
       })
     }
+    fromFile(fileLocation)
+  }
+
+  def getMeaningAtIndex(locus: Int): String = {
+    log info(s"locus: $locus")
+    take(locus - 1)
+    val (_, meaning_line) = next()
+    fromFile(fileLocation)
+    return meaning_line
+  }
+
+  def getMeanings(word: String): ListBuffer[String] = {
+    if (wordToLocations.size == 0) {
+      makeWordToLocationMap()
+    }
+    val definition_locus_list = wordToLocations.getOrElse(word, ListBuffer[Int]())
+    return definition_locus_list.map(getMeaningAtIndex(_))
+  }
+}
+
+object babylonDictTest {
+  val log = LoggerFactory.getLogger(this.getClass)
+
+  def main(args: Array[String]) {
+    val kalpadruma = new BabylonDictionary(name_in = "कल्पद्रुमः", source_in = "http://www.sanskrit-lexicon.uni-koeln.de/scans/csldoc/contrib/index.html")
+    kalpadruma.fromFile(infileStr = "/home/vvasuki/stardict-sanskrit/sa-head/kalpadruma-sa/kalpadruma-sa.babylon_final")
+    log info kalpadruma.getMeanings("अ").mkString("\n\n")
   }
 }
--- src/main/scala/sanskritnlp/wiki/bot/wiktionary.scala
+++ src/main/scala/sanskritnlp/wiki/bot/wiktionary.scala
@@ -1,35 +1,15 @@
 package sanskritnlp.wiki.bot
 
+import org.slf4j.LoggerFactory
 import sanskritnlp.dictionary.BabylonDictionary
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object wiktionary extends wikiBot {
+  override val log = LoggerFactory.getLogger(this.getClass)
   override val languageCode = "sa"
   override val wikiSiteName = "wiktionary"
-
-  def addDictionaryMeaning(headwords: Array[String], meaning: String, dictionary: BabylonDictionary) = {
-    val dict_source = dictionary.source
-    log.info(headwords.mkString(","))
-    val head_text = s"{{फलकम्:यन्त्रशोधितकोशार्थः|कोशमूलम् = $dict_source}}"
-
-    val sectionPath = s"/यन्त्रोपारोपितकोशांशः/${dictionary.dict_name}"
-    val category_name = sectionPath.split('/').filterNot(_ == "").mkString("-")
-    val tail_text = s"[[वर्गः: $category_name]]"
-
-    val section_text = s"$meaning\n\n$tail_text"
-
-    headwords.foreach(head => {
-      if (dictionary.wordsSeen contains head) {
-        appendToSection(title = head, sectionPath = sectionPath, summary = "अर्थनिवेशः", text = s"\n\n$section_text" )
-      } else {
-        replaceSectionText(title = head, sectionPath = sectionPath, summary = "अर्थनिवेशः", text = s"$head_text\n\n$section_text")
-      }
-      // log info section_text
-    })
-    dictionary.wordsSeen ++=  headwords
-  }
 
   def replaceBadText(headwords: Array[String], regexMap: Map[String, String]) = {
     log.info(headwords.mkString(","))
@@ -38,25 +18,55 @@ object wiktionary extends wikiBot {
     })
   }
 
+  def addDictionaryMeaning(head: String, meaning: String, dictionary: BabylonDictionary) = {
+    val dict_source = dictionary.source
+    log.info(s"Adding $head")
+    val head_text = s"{{फलकम्:यन्त्रशोधितकोशार्थः|कोशमूलम् = $dict_source}}"
+
+    val sectionPath = s"/यन्त्रोपारोपितकोशांशः/${dictionary.dict_name}"
+    val category_name = sectionPath.split('/').filterNot(_ == "").mkString("-")
+    val tail_text = s"[[वर्गः: $category_name]]"
+
+    val section_text = s"$meaning\n\n$tail_text"
+    if (dictionary.wordsSeen contains head) {
+      appendToSection(title = head, sectionPath = sectionPath, summary = "अर्थनिवेशः", text = s"\n\n$section_text" )
+    } else {
+      replaceSectionText(title = head, sectionPath = sectionPath, summary = "अर्थनिवेशः", text = s"$head_text\n\n$section_text")
+    }
+    // log info section_text
+    dictionary.wordsSeen += head
+  }
+
   def deleteSection(headwords: Array[String], sectionPath: String): Unit = {
     headwords.foreach(head => {
       deleteSection(head, sectionPath)
     })
   }
 
-  def uploadFromBabylonDictsSerial(dictList: List[BabylonDictionary], start_word_index: Int = 1, end_word_index: Int = 100000) = {
-    dictList.foreach(dictionary => {
-      var word_index = 0
-      while (dictionary.hasNext() && word_index + 1 <= end_word_index) {
-        val (headwords, meaning) = dictionary.next()
-        word_index = word_index + 1
-        log.info(s"word_index : $word_index")
-        if (word_index >= start_word_index) {
-          // log.info(headwords.mkString(","))
-          val sktHeadwords = headwords.filter(_ matches "\\p{IsDevanagari}+")
-          addDictionaryMeaning(sktHeadwords, meaning, dictionary)
-        }
+  def uploadFromBabylonDict(dictionary: BabylonDictionary, headword_pattern: String = "\\p{IsDevanagari}+") = {
+    dictionary.word_index = 0
+    while (dictionary.hasNext() && dictionary.word_index + 1 <= dictionary.end_word_index) {
+      val (headwords, meaning) = dictionary.next()
+      dictionary.word_index = dictionary.word_index + 1
+      log.info(s"${dictionary.dict_name} word_index : ${dictionary.word_index}")
+      if (dictionary.word_index >= dictionary.start_word_index) {
+        // log.info(headwords.mkString(","))
+        val sktHeadwords = headwords.filter(_ matches headword_pattern)
+        sktHeadwords.foreach(addDictionaryMeaning(_, meaning, dictionary))
       }
+    }
+  }
+
+  def uploadFromBabylonDictsSerial(dictList: List[BabylonDictionary]) = {
+    dictList.foreach(dictionary => {
+      uploadFromBabylonDict(dictionary)
+    })
+  }
+
+
+  def uploadFromBabylonDictsCombined(dictList: List[BabylonDictionary]) = {
+    dictList.foreach(dictionary => {
+
     })
   }
 
@@ -75,7 +85,6 @@ object wiktionary extends wikiBot {
     apte.fromFile(infileStr = "/home/vvasuki/stardict-sanskrit/sa-head/apte-sa/apte-sa.babylon_final")
     val kRdanta = new BabylonDictionary(name_in = "Apte", source_in = "http://www.sanskrit-lexicon.uni-koeln.de/scans/csldoc/contrib/index.html")
     kRdanta.fromFile(infileStr = "/home/vvasuki/stardict-sanskrit/sa-head/kRdanta-rUpa-mAlA/kRdanta-rUpa-mAlA.babylon_final")
-    uploadFromBabylonDicts(List(vAcas, mw, apte),
-      start_word_index = 51, end_word_index =  100000)
+    uploadFromBabylonDictsSerial(List(vAcas, mw, apte))
   }
 }
